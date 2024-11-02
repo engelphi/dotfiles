d0e091c5d0fa8ef13b9e1eba11607cc28bb482fe
Philipp Engel <philipp.engel.1990@googlemail.com>
2024-11-02 15:01:08 +0100

    remove old config

--- config/nvim/lua/user/lsp/settings/clangd.lua
+++
@@ -1,3 +0,0 @@
-return {
-    cmd = { "clangd", "--header-insertion=never", "--suggest-missing-includes","-all-scopes-completion", "-completion-style=detailed", "-function-arg-placeholders", "-j=2", "-log=verbose", "-background-index",  "-cross-file-rename" }
-}
--- config/nvim/lua/user/lsp/settings/cmake.lua
+++
@@ -1,8 +0,0 @@
-return {
-    cmd = { "cmake-language-server" },
-    filetypes = { "cmake" },
-    init_options = {
-        buildDirectory = "build"
-    },
-    single_file_support = true
-}
