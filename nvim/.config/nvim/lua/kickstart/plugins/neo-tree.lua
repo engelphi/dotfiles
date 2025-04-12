9a91999bd4633237cc74c05c8cf97420226f853e
Philipp Engel <philipp.engel.1990@googlemail.com>
2025-04-12 13:22:44 +0200

    feat: add snacks.nvim and folds

--- nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,26 +2,26 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-    { '<leader>e', ':Neotree reveal<CR>', { desc = 'NeoTree toggle' } },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['<leader>e'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree toggle<CR>", { desc = "NeoTree reveal" } },
+		{ "<leader>e", ":Neotree toggle<CR>", { desc = "NeoTree toggle" } },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+					["<leader>e"] = "close_window",
+				},
+			},
+		},
+	},
 }
